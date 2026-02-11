6bdf0f87b32ebe00f26827e038b92d3153b312de
Ulas Bardak <bardak@gmail.com>
2026-02-10 23:43:57 -0700

    Improved performance via deque for Part 1

--- 2016/day_19/presents.py
+++ 2016/day_19/presents.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 NUM_ELVES = 3001330
 #NUM_ELVES = 5
 
@@ -10,25 +12,21 @@ class Elf:
         return f"Elf {self.id} with {self.presents} presents"
 
 def solve1(elves: list[Elf]) -> int:
-    to_be_removed_idx = -1  
-    starting_point = 0
-    while len(elves) > 1:
-        if(len(elves) % 1000 == 0):
-            print(len(elves))
-        for i in range(starting_point,len(elves)):
-            # Go through each elf and steal presents from the next elf
-            if i == len(elves) - 1:
-                # elves[i].presents += elves[0].presents
-                to_be_removed_idx = 0
-                starting_point = 0
-            else:
-                # elves[i].presents += elves[i + 1].presents
-                to_be_removed_idx = i + 1
-                starting_point = i + 1
-            break
-        elves.pop(to_be_removed_idx)
-    print(elves[0])
-    exit()  
+    elves_q = deque(elves)
+    
+    while len(elves_q) > 1:
+        elves_q.rotate(-1)
+        elves_q.popleft()        
+    return elves_q[0].id
+
+def solve2(elves: list[Elf]) -> int:
+    elves_q = deque(elves)
+    
+    while len(elves_q) > 1:
+        elves_q.rotate(len(elves_q)//2)
+        elves_q.popleft()        
+    return elves_q[0].id
 
 elves = [Elf(i) for i in range(1, NUM_ELVES + 1)]
-solve1(elves)
+print("ID of the elf with all the presents: ", solve1(elves))
+print("ID of the elf with all the presents: ", solve2(elves))
